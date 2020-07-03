e093d696b3cd2184f1333bc4cdd97177ada01ee8
rohitrox444 <rohitrox444@gmail.com>
2020-07-03 14:48:26 +0530

    tensorflow error resolved

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+import json
+import random
+import tensorflow
+import tflearn
+import numpy
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
-import tflearn
-import tensorflow
-import random
 
-import json
+with open('intents.json') as file:
+    data = json.load(file)
+
+print(data['intents'])
+# Worked till here but need to solve some unknown erorrs might be arising because of system configuration
+
+
+
+
+# words = []
+# labels = []
+# docs_x = []
+# docs_y = []
+
+# for intent in data['intents']:
+#     for pattern in intent['patterns']:
+#         wrds = nltk.word_tokenize(pattern)
+#         words.extend(wrds)
+#         docs_x.append(wrds)
+#         docs_y.append(intent["tag"])
+
+#     if intent['tag'] not in labels:
+#         labels.append(intent['tag'])
